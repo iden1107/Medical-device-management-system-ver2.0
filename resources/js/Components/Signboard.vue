642581aee3f54f0e0159f85de1e3699a59cbff7b
Kamino <iden1107@icloud.com>
2023-03-19 17:25:28 +0900

    ログイン画面途中

---
+++ resources/js/Components/Signboard.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+
+</script>
+
+<template>
+        <div class="w-full sm:max-w-md mx-auto mt-6 px-6 py-4 bg-Emerald-400 shadow-md h-40">
+            <div class="flex">
+                <img :src="'img/logo.png'" alt="">
+                <div class="pl-2">
+                    <h2 class="text-2xl text-white">医療法人 サンプル</h2>
+                    <h1 class="text-h4 text-white ff">サンプル病院</h1>
+                </div>
+            </div>
+        </div>
+</template>
+
+<style scoped>
+.ff{
+    font-family: 'Shippori Mincho', serif !important;
+    font-weight: 900;
+    font-size: 48px;
+    color: #FFF;
+}
+img{
+    height: 120px;
+}
+</style>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,18 +1,35 @@
 <script setup>
+import { ref } from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import Signboard from '@/Components/Signboard.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
 import { Link } from '@inertiajs/vue3';
+
+const showingNavigationDropdown = ref(false);
+const drawer = ref(false)
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+    <div class="min-h-screen bg-gray-100 pb-0 ">
+        <!-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"> -->
+        <nav class="bg-Emerald-400  drop-shadow">
+            <!-- Primary Navigation Menu -->
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex justify-center h-10">
+                    <div class="flex text-white items-center font-medium">医療機器管理システム</div>
+                </div>
+            </div>
+        </nav>
 
+        <!-- login component -->
+        <Signboard class="w-20 h-20 fill-current text-gray-500" />
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-md mx-auto mt-6 px-6 py-4 bg-white shadow-md overflow-hidden"
         >
             <slot />
         </div>
